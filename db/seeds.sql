af53275244420142b8be3323a6d4f781e8879f6a
Jack Sorensen <jack.sorensen11@gmail.com>
2020-01-16 12:38:28 -0800

    first github push, working application

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Vegan Burger');
+INSERT INTO cats (name) VALUES ('Sloppy Joe');
+INSERT INTO cats (name, sleepy) VALUES ('PB Bacon Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Double Meat Double Cheese', true);
+INSERT INTO cats (name, sleepy) VALUES ('Single With all the fixins', true);
+INSERT INTO cats (name) VALUES ('AMPM Burger thats been sitting there for 3 hours');
